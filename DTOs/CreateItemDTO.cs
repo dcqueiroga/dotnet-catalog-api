8498c2cdf4c2f96eaeeddc87eaecf028a1585710
Daniel <dcqueiroga@gmail.com>
2022-01-13 16:02:49 -0300

    Adding Data Annotations to CreateItemDTO

--- DTOs/CreateItemDTO.cs
+++ DTOs/CreateItemDTO.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Catalog.DTOs
 {
     public class CreateItemDTO
-    {   public string Name { get; init; }
+    {
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        [Range(1, 1000)]
         public decimal Price { get; init; }
     }
 }
