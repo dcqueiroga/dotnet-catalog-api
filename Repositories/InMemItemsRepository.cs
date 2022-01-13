1dabc560fafd7321431c9caa42cc0bb6c2b1838e
Daniel <dcqueiroga@gmail.com>
2022-01-13 14:18:31 -0300

    Implementing the GET route

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories {
+    public class InMemItemsRepository {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Portion", Price = 9, CreatedDate = System.DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = System.DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Chield", Price = 18, CreatedDate = System.DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems() {
+            return items;
+        }
+
+        public Item GetItem(Guid Id) {
+            return items.Where(item => item.Id == Id).SingleOrDefault();
+        }
+    }
+}
