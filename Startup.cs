d3af408722da21dd3e3457d016f54f0d9cd8dc1d
Daniel <dcqueiroga@gmail.com>
2022-01-18 19:55:11 -0300

    Adding health check endpoint

--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,8 @@ namespace Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +67,7 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
